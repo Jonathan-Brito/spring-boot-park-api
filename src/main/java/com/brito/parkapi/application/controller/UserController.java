aa55d240dae2008613cfd8d6234373a178e13562
Jonathan Brito Costa <devjonathanbrito@gmail.com>
2024-01-07 18:55:42 -0300

    Criando recurso para criar um novo usuário

--- src/main/java/com/brito/parkapi/application/controller/UserController.java
+++ src/main/java/com/brito/parkapi/application/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.brito.parkapi.application.controller;
 
+import com.brito.parkapi.domain.model.User;
 import com.brito.parkapi.domain.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    // ResponseEntity emcapsula o objeto(User) para ser transformado em um Json  e enviado para o cliente com a resposta da requisição
+    // Juntamente ao objeto ele guarda algumas informações como código de restposta e cabeçalho
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user){ // O User é o argumento devido a aplicação cliente tem um Json com os campos do Obejto User
+                                                    // Por isso os campos de ambos deve ser igual para er feito a conversão
+        User users = userService.save(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(users);
+    }
 }
--- src/main/java/com/brito/parkapi/domain/service/UserService.java
+++ src/main/java/com/brito/parkapi/domain/service/UserService.java
@@ -1,12 +1,19 @@
 package com.brito.parkapi.domain.service;
 
+import com.brito.parkapi.domain.model.User;
 import com.brito.parkapi.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor // Cria um metodo construtor com a variável para fazer a injeção de dependencias
 @Service // Bean gerenciado pelo spring
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Transactional // Spring vai cuidar
+    public User save(User user){
+        return userRepository.save(user);
+    }
 }
